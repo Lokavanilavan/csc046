33e6c6c206cd08d8ee48de7120230c20def5e824
124 <123@gmail.com>
2020-09-18 05:10:34 +0530

    branches

--- Examples.java
+++ Examples.java
@@ -7,17 +7,17 @@ public class CSCExamples{
 	
 	public void setquesNo()
 	{
-		
+		this.quesNo = quesNo;
 	}
 	
 	public void set category()
 	{
-		
+		this.category = category;
 	}
 	
 	public void set text()
 	{
-		
+		this.text = text;
 	}
 	
 }
