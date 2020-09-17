97d4de61b4170d03854522c1def32cca4748e4bd
124 <123@gmail.com>
2020-09-18 04:16:11 +0530

    CSCExamples.java

---
+++ Examples.java
@@ -0,0 +1,23 @@
+public class CSCExamples{
+	
+	public int quesNo;
+	public String category;
+	public String text;
+	
+	
+	public void setquesNo()
+	{
+		
+	}
+	
+	public void set category()
+	{
+		
+	}
+	
+	public void set text()
+	{
+		
+	}
+	
+}
